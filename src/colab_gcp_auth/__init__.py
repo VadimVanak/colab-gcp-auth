f90c281b1940c376acae4debb05a64ac6d241356
VadimVanak <148138618+VadimVanak@users.noreply.github.com>
2025-12-31 13:17:32 +0100

    Update __init__.py

--- src/colab_gcp_auth/__init__.py
+++ src/colab_gcp_auth/__init__.py
@@ -1,3 +1,3 @@
-from .auth import gcp_connect, get_secret_via_gcloud
+from .auth import gcp_connect, get_secret_via_gcloud, get_argv
 
-__all__ = ["gcp_connect", "get_secret_via_gcloud"]
+__all__ = ["gcp_connect", "get_secret_via_gcloud", "get_argv"]
